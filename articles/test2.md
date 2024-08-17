c0c6b1be1c2417c2963ba88901262fadcd514105
Maël Naccache Tüfekçi <contact@maeln.com>
2024-08-17 21:27:51 +0200

    add templating

---
+++ articles/test2.md
@@ -0,0 +1,11 @@
+# A title
+
+A test with *bold stuff* and also /italics text/.
+And add to this a [link](https://maeln.com).
+
+|We also got *sidenote* here!|
+
+# Should be enough
+|Don't ya think?|
+
+bye.
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::env;
 use std::fs;
+
+use minijinja::{context, Environment};
 
 use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[grammar = "maeldown.pest"]
@@ -21,27 +25,28 @@ fn generate(token: Pair<Rule>) -> String {
             buf += token.as_str();
         }
         Rule::text_block => {
+            buf += "<p>";
             let tk_lst: Vec<String> = token.into_inner().map(generate).collect();
             buf += &tk_lst.join("");
+            buf += "</p>";
         }
         Rule::bold_text => {
-            buf += "<b>";
+            buf += "<span class=\"bold\">";
             let tk_lst: Vec<String> = token.into_inner().map(generate).collect();
             buf += &tk_lst.join("");
-            buf += "</b>";
+            buf += "</span>";
         }
         Rule::italic_text => {
-            buf += "<i>";
+            buf += "<span class=\"italic\">";
             let tk_lst: Vec<String> = token.into_inner().map(generate).collect();
             buf += &tk_lst.join("");
-            buf += "</i>";
+            buf += "</span>";
         }
         Rule::sidenote => {
-            println!("DEG: {:?}", token);
-            buf += "<sidenote>";
+            buf += "<div class=\"sidenote\">";
             let tk_lst: Vec<String> = token.into_inner().map(generate).collect();
             buf += &tk_lst.join("");
-            buf += "</sidenote>";
+            buf += "</div>";
         }
         Rule::link => {
             let mut inner = token.into_inner();
@@ -59,9 +64,7 @@ fn generate(token: Pair<Rule>) -> String {
         Rule::link_url => {
             buf += token.as_str();
         }
-        Rule::EOI => {
-            println!("EOI");
-        }
+        Rule::EOI => {}
         _ => {
             println!("Unr: {:?}", token);
         }
@@ -70,8 +73,8 @@ fn generate(token: Pair<Rule>) -> String {
     buf
 }
 
-fn main() {
-    let unparsed_file = fs::read_to_string("test.md").expect("Could not read file");
+fn parse_file(path: &str) -> String {
+    let unparsed_file = fs::read_to_string(path).expect("Could not read file");
     let parsed = MDParser::parse(Rule::document, &unparsed_file)
         .expect("could not parse")
         .next()
@@ -81,5 +84,40 @@ fn main() {
     for line in parsed.into_inner() {
         document += &generate(line);
     }
-    println!("{}", document);
+    document
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        println!("not enough argument: <template file> <article directory>");
+        return;
+    }
+
+    // Load template
+    let template_str = fs::read_to_string(&args[1]).expect("Could not read template file.");
+    let mut env = Environment::new();
+    env.add_template("base", &template_str).unwrap();
+    let template = env.get_template("base").unwrap();
+
+    // Parse all the articles
+    let mut articles: Vec<String> = Vec::new();
+    for entry in WalkDir::new(&args[2]).follow_links(true) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+        if path.is_file() && path.extension().and_then(|s| s.to_str()) == Some("md") {
+            let article = fs::read_to_string(path);
+            if let Err(e) = article {
+                println!("Error while reading {}: {}", path.display(), e);
+                continue;
+            }
+            let parsed = parse_file(path.to_str().unwrap());
+            articles.push(parsed);
+        }
+    }
+
+    // Render everything
+    // TODO: Add metadata to maeldown and sort article by date.
+    println!("{}", template.render(context! { title => "Wololo", articles => articles }).unwrap());
 }
